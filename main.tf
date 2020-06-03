193063e6546530351b66f2cb70b8ab4d8f5505b8
wolfkabal <wolfk@Yoda-1.bioserge.lan>
2020-06-03 00:26:12 -0400

    asg updates

--- main.tf
+++ main.tf
@@ -9,16 +9,40 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balance"
 }
 
 
+data "aws_vpc" "default" {
+  default = true
+}
 
-resource "aws_instance" "example" {
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id 
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 5
+
+  tag {
+      key                 = "Name"
+      value               = "terraform-asg-example"
+      propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_configuration" "example" {
   #ami = "ami-09d95fab7fff3776c" # Amazon Linux 2 AMI (HVM) us-east-1
-  image = "ami-026dea5602e368e96"  # Amazon Linux 2 AMI (HVM) us-east-2
+  image_id = "ami-026dea5602e368e96"  # Amazon Linux 2 AMI (HVM) us-east-2
   instance_type = "t2.micro"
   #subnet_id = "subnet-9de57eb6"
   security_groups = [aws_security_group.instance.id]
@@ -34,10 +58,6 @@ resource "aws_instance" "example" {
   lifecycle {
       create_before_destroy = true
   }
-
-  tags = {
-      Name = "terraform-example1"
-  }
 }
 
 resource "aws_security_group" "instance" {
@@ -51,3 +71,78 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_lb" "example" {
+  name                    = "terraform-asg-example"
+  load_balancer_type      = "application"
+  subnets                 = data.aws_subnet_ids.default.ids
+  security_groups         = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port              = 80
+    protocol          = "HTTP"
+
+    # By default, return a simple 404 page
+    default_action {
+        type = "fixed-response"
+
+        fixed_response { 
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code  = 404
+        }
+    }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress { 
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name      = "terraform-asg-example"
+  port      = var.server_port
+  protocol  = "HTTP"
+  vpc_id    = data.aws_vpc.default.id
+
+  health_check {
+      path                 = "/"
+      protocol             = "HTTP"
+      matcher              = "200"
+      interval             = 15
+      timeout              = 3
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+      field  = "path-pattern"
+      values = ["*"]
+  }
+
+  action {
+      type             = "forward"
+      target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
